06ece202070c866e3666baa42a1ae76f33b02dbc
Pedro Jorquera <pjorquera@izertis.com>
2024-10-06 10:28:06 +0200

    Create database and user with Sync Gateway API

--- README.md
+++ README.md
@@ -6,4 +6,8 @@ AI Sync Demo App
 ```
 cd back
 docker compose up
+
+curl -u 'admin:zxc123' --request PUT http://localhost:4985/aisync/ --header 'Content-Type: application/json' --data-raw '{ "bucket": "aisync", "num_index_replicas": 0 }'
+
+curl -u 'admin:zxc123' --request POST http://localhost:4985/aisync/_user/ -H 'Content-Type: application/json' -d '{ "name": "admin", "password": "zxc123" }'
 ```
--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import PouchDB from 'pouchdb-browser';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,5 +11,29 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  private db = new PouchDB('http://localhost:4984/aisync', { auth: { username: 'admin', password: 'zxc123' } });
+
   title = 'front';
+
+  constructor() {
+
+    console.log('Started');
+    // console.log(JSON.stringify(this.db));
+
+    this.db.info().then(value => {
+      console.log('info');
+      console.log(JSON.stringify(value));
+    });
+
+    this.db.allDocs({ include_docs: true }).then(docs => {
+      console.log('allDocs');
+      console.log(JSON.stringify(docs));
+    });
+
+    this.db.changes({ live: true, since: 'now', include_docs: true }).on('change', value => {
+      console.log(JSON.stringify(value));
+    });
+  }
+
 }
